2699797e1c0d7f142afc3139873d49404dcd24ad
LITES <liunlong@gmail.com>
2026-03-29 10:29:23 +0800

    fix build

--- android/app/src/main/kotlin/com/appshub/bettbox/plugins/AppPlugin.kt
+++ android/app/src/main/kotlin/com/appshub/bettbox/plugins/AppPlugin.kt
@@ -3,6 +3,7 @@ package com.appshub.bettbox.plugins
 import android.Manifest
 import android.app.Activity
 import android.app.ActivityManager
+import android.content.Context
 import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.ComponentInfo
